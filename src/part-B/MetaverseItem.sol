330dbbfbdea3e1173764b8625962a680f5a31ef0
Kyrylo Protchenko <32849961+kprotchenko@users.noreply.github.com>
2025-11-07 15:21:31 -0500

    Additional contract changes for Part-B

--- src/part-B/MetaverseItem.sol
+++ src/part-B/MetaverseItem.sol
@@ -6,19 +6,49 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Royalty.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-// B-1: MetaverseItem inherits ERC721, ERC721Royalty, AccessControl, ERC721Enumerable.
+// B-1: MetaverseItem inherits ERC721, ERC721Royalty, ERC721Enumerable, AccessControl.
 contract MetaverseItem is ERC721, ERC721Royalty, ERC721Enumerable, AccessControl {
-    string public immutable baseURI;
-    address public immutable admin;
-    uint96 public immutable royalty;
+    address public admin;
+    uint96 public royalty;
+    uint16 public constant MAX_SUPPLY = 10_000;
+    uint256 private _nextTokenId;
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    string public immutable baseURI;
+
+
 
     // B-2: Constructor (name, symbol, baseURI, admin) sets default 5 % royalty and grants MINTER_ROLE to admin.
     constructor(string memory name_, string memory symbol_, string memory _baseURI, address _admin) ERC721(name_, symbol_) {
         baseURI = _baseURI;
         admin = _admin;
         royalty = 500;
+        _grantRole(DEFAULT_ADMIN_ROLE, admin);
     }
+
+    // B-3: mint(address to) – only minter; tokenId auto-increments; max supply = 10 000.
+    function mint(address to) external onlyRole(MINTER_ROLE) {
+        require(totalSupply() < MAX_SUPPLY, "Max supply reached");
+        _safeMint(to, ++_nextTokenId);
+        _setTokenRoyalty(_nextTokenId, msg.sender, royalty);
+    }
+
+    // B-4: setBaseURI(string) – only admin; stores IPFS base (e.g., ipfs://CID/).
+    function setBaseURI(string memory _baseURI) external onlyRole(DEFAULT_ADMIN_ROLE){
+        baseURI = _baseURI;
+    }
+
+    // B-5.1: Override _baseURI();.
+    function _baseURI() internal override(ERC721) view returns (string memory) {
+        return baseURI;
+    }
+
+    // B-5.2: tokenURI = baseURI + tokenId + “.json”.
+    function tokenURI(uint256 tokenId) public override(ERC721) view returns (string memory) {
+        _requireOwned(tokenId);
+        string memory baseURI = _baseURI();
+        return bytes(baseURI).length > 0 ? string.concat(baseURI, tokenId.toString(), ".json") : "";
+    }
+
 
     function _increaseBalance(address account, uint128 amount) internal override(ERC721, ERC721Enumerable) {
         super._increaseBalance(account, amount);
@@ -40,6 +70,4 @@ contract MetaverseItem is ERC721, ERC721Royalty, ERC721Enumerable, AccessControl
     {
         return super.supportsInterface(interfaceId);
     }
-
-
 }
